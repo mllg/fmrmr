69535eb9cf5aa3df1b781131c5d0a4e2549cd73e
Michel Lang <lang@statistik.tu-dortmund.de>
2013-05-08 13:55:23 +0200

    todo

--- src/cindex.cpp
+++ src/cindex.cpp
@@ -28,24 +28,20 @@ SEXP cindex(SEXP R_time, SEXP R_status, SEXP R_x) {
     const NumericVector time(R_time);
     const LogicalVector status(R_status);
     const NumericMatrix x(R_x);
+    const size n = x.nrow(), p = x.ncol();
 
-    if (is_true(any(is_na(time))) || is_true(any(is_na(status)))) {
-        exception("Survival time or censoring indicator contain missing values");
-        return R_NilValue;
-    }
-    if (is_true(all(!status))) {
-        exception("No uncensored observation");
-        return R_NilValue;
-    }
-    if (is_true(any(is_na(x)))) {
-        exception("Feature matrix contains missing values");
-        return R_NilValue;
-    }
+    if (is_true(any(is_na(time))) || is_true(any(is_na(status))))
+        Rf_error("Survival time or censoring indicator contain missing values");
+    if (is_true(all(time(0) == tail(time, n - 1))))
+        Rf_error("All times are identical");
+    if (is_true(all(!status)))
+        Rf_error("No uncensored observation");
+    if (is_true(any(is_na(x))))
+        Rf_error("Feature matrix contains missing values");
 
     const IntegerVector ord = order(time);
     IntegerVector::iterator i, j;
     IntegerVector::const_iterator iend = ord.end() - 1;
-    const size p = x.ncol();
 
     size counter = 0;
     NumericVector res(p, 0);
@@ -57,6 +53,7 @@ SEXP cindex(SEXP R_time, SEXP R_status, SEXP R_x) {
             if (time(*i) == time(*j))
                 break;
             counter++;
+            // TODO this could perhaps be vectorized
             for (size k = 0; k < p; k++) {
                 if (x(*j, k) > x(*i, k)) {
                     res(k) += 1.0;
@@ -67,7 +64,8 @@ SEXP cindex(SEXP R_time, SEXP R_status, SEXP R_x) {
         }
     }
 
-    //if (counter == 0) ..
+    if (counter == 0)
+        Rf_error("Internal error: Unable to calculate concordance index");
     res = res / counter;
     return wrap(res);
 }
